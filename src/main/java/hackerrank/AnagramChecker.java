df5282f142c8d7de51c333001b634615af963388
gurkaransingh <singhgurkaran025@gmail.com>
2025-03-20 05:35:37 +0530

    Hackerrank to check if 2 strings are anagrams -  added hackerrank solution

--- src/main/java/hackerrank/AnagramChecker.java
+++ src/main/java/hackerrank/AnagramChecker.java
@@ -1,5 +1,6 @@
 package hackerrank;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class AnagramChecker {
@@ -14,17 +15,34 @@ public class AnagramChecker {
         // Complete the function
         if (a.length() != b.length()) return false;
 
-        HashMap<Character, Integer> freqMap = new HashMap<>();
-        for (char c : a.toCharArray()) {
-            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);//complexity O(n) for each loop
+//        HashMap<Character, Integer> freqMap = new HashMap<>();
+//        for (char c : a.toCharArray()) {
+//            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);//complexity O(n) for each loop
+//        }
+//
+//        for (char c : b.toCharArray()) {
+//            if (!freqMap.containsKey(c) || freqMap.get(c) == 0)
+//                return false;
+//
+//            freqMap.put(c, freqMap.get(c) - 1);
+//        }
+
+        int[] freq = new int[26];
+
+        // Single loop: Increment for str1 and decrement for str2 simultaneously
+        for (int i = 0; i < a.length(); i++) {
+            freq[a.toLowerCase().charAt(i) - 'a']++;
+            freq[b.toLowerCase().charAt(i) - 'a']--;
         }
 
-        for (char c : b.toCharArray()) {
-            if (!freqMap.containsKey(c) || freqMap.get(c) == 0)
+        System.out.println(Arrays.toString(freq));
+        // Check if all counts are zero
+        for (int count : freq) {
+            if (count != 0) {
                 return false;
-
-            freqMap.put(c, freqMap.get(c) - 1);
+            }
         }
+
         return true;
 
 
