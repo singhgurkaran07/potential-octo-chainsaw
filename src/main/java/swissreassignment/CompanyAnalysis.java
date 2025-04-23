4bc07d0ee32d8f090bb173f8ee4f74c139321f9a
gurkaransingh <singhgurkaran025@gmail.com>
2025-04-23 18:17:38 +0530

    swiss Re assignment task

--- src/main/java/swissreassignment/CompanyAnalysis.java
+++ src/main/java/swissreassignment/CompanyAnalysis.java
@@ -1,4 +1,4 @@
-package swissreass;
+package swissreassignment;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
--- src/main/java/swissreassignment/Employee.java
+++ src/main/java/swissreassignment/Employee.java
@@ -1,4 +1,4 @@
-package swissreass;
+package swissreassignment;
 
 import lombok.Getter;
 
